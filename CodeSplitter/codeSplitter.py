291521505fae4d7cb297808ce34d987276e3a065
maikeljh <mjh191003@gmail.com>
2022-11-17 16:37:28 +0700

    fix grammar

--- CodeSplitter/codeSplitter.py
+++ CodeSplitter/codeSplitter.py
@@ -17,7 +17,7 @@ def Code_splitter(inputFile):
         hasil_output = re.split('([(|)|;|-|+|*|/|%|!|=|<|>|&|?|~|^|0|1|2|3|4|5|6|7|8|9])', isi_output)
         for isi_hasil_output1 in hasil_output :
             if (isi_hasil_output1 != "break" and isi_hasil_output1 != 'const' and isi_hasil_output1 != 'case' and isi_hasil_output1 != 'catch' and isi_hasil_output1 != 'continue' and isi_hasil_output1 != 'default' and isi_hasil_output1 != 'delete' and isi_hasil_output1 != 'else' and isi_hasil_output1 != 'false' and isi_hasil_output1 != 'finally' and isi_hasil_output1 != "for" and isi_hasil_output1 != 'function' and isi_hasil_output1 != 'if' and isi_hasil_output1 != 'let' and isi_hasil_output1 != 'null' and isi_hasil_output1 != 'return' and isi_hasil_output1 != 'switch' and isi_hasil_output1 != 'throw' and isi_hasil_output1 != 'try' and isi_hasil_output1 != 'true' and isi_hasil_output1 != 'var' and isi_hasil_output1 != 'while' ):
-                hasil_output2 = re.split('(a|b|c|d|e|f|g|h|i|j|k|l|m|n|o|p|q|r|s|t|u|v|w|x|y|z)', isi_hasil_output1)
+                hasil_output2 = re.split('(a|b|c|d|e|f|g|h|i|j|k|l|m|n|o|p|q|r|s|t|u|v|w|x|y|z|A|B|C|D|E|F|G|H|I|J|K|L|M|N|O|P|Q|R|S|T|U|V|W|X|Y|Z)', isi_hasil_output1)
                 for isi_hasil_output2 in hasil_output2 :
                     if (isi_hasil_output2 !=''):
                         outputfix.append(isi_hasil_output2)
--- main.py
+++ main.py
@@ -3,8 +3,9 @@ from CodeSplitter import codeSplitter as cs
 from CFG import GrammerReader as cfg
 
 CNF = cfg.convertCFGtoCNY()
-for item in CNF.items():
-    print(item)
+#for key, value in CNF.items():
+    #print(key, ' : ', value)
+
 output = cs.Code_splitter("./test.js")
 print(output)
 cyk.CYK(output, CNF)
