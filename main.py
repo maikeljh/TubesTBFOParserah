54f2e688fd3394c6f21f3c1acf026b2caa082e19
maikeljh <mjh191003@gmail.com>
2022-11-20 13:12:57 +0700

    handle file not found and add new test case

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 from CYK import cyk as cyk
-from CodeSplitter import codeSplitter as cs
+from CodeSplitter import tempCodeSplitter as cs
 from CFG import GrammerReader as cfg
 
 print("Loading...")
+
 CNF = cfg.convertCFGtoCNY()
+
 print("\nWelcome To Our Javascript Parser!")
+
 file = str(input("Please Input Your File Javascript (type EXIT to exit): "))
+
 while(file != "EXIT"):
     if (not(".js" in file)):
         file += ".js"
+    try:
+        output = cs.Code_splitter("./" + file)
+        print(output)
+        cyk.CYK(output, CNF)
+    except:
+        print("\nFile not found.")
 
-    output = cs.Code_splitter("./" + file)
-    print(output)
-    cyk.CYK(output, CNF)
     file = str(input("\nPlease Input Your File Javascript (type EXIT to exit): "))
+    
 print("\nThank You! Have A Nice Day :)")
