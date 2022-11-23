8103541691b8593d2984e637e8b95701db27911b
Raylouiss <sebastianraynard@gmail.com>
2022-11-24 01:39:22 +0700

    update codesplit

--- CodeSplitter/codeSplitter.py
+++ CodeSplitter/codeSplitter.py
@@ -91,7 +91,7 @@ def Code_splitter(inputFile):
             spasi1 = False
         idx +=1
     for isi_output in  output:
-        hasil_output = re.split('([(|)|;|-|+|*|/|%|!|,|=|<|>|&|?|~|^|0|1|2|3|4|5|6|7|8|9|:|{|}|`|\\\|"|\'|$])', isi_output) # Memisahkan tanda dan angka
+        hasil_output = re.split('([(|)|;|-|+|*|/|%|!|,|=|<|>|&|?|~|^|0|1|2|3|4|5|6|7|8|9|:|{|}|`|\\\|"|\'|$|_])', isi_output) # Memisahkan tanda dan angka
         for isi_hasil_output1 in hasil_output : #memisahkan huruf jika tidak termasuk fungsi
             if (isi_hasil_output1 != "break" and isi_hasil_output1 != 'const' and isi_hasil_output1 != 'case' and isi_hasil_output1 != 'catch' and isi_hasil_output1 != 'continue' and isi_hasil_output1 != 'default' and isi_hasil_output1 != 'delete' and isi_hasil_output1 != 'else' and isi_hasil_output1 != 'false' and isi_hasil_output1 != 'finally' and isi_hasil_output1 != "for" and isi_hasil_output1 != 'function' and isi_hasil_output1 != 'if' and isi_hasil_output1 != 'let' and isi_hasil_output1 != 'null' and isi_hasil_output1 != 'return' and isi_hasil_output1 != 'switch' and isi_hasil_output1 != 'throw' and isi_hasil_output1 != 'try' and isi_hasil_output1 != 'true' and isi_hasil_output1 != 'var' and isi_hasil_output1 != 'while' and isi_hasil_output1!='default' ):
                 hasil_output2 = re.split('(a|b|c|d|e|f|g|h|i|j|k|l|m|n|o|p|q|r|s|t|u|v|w|x|y|z|A|B|C|D|E|F|G|H|I|J|K|L|M|N|O|P|Q|R|S|T|U|V|W|X|Y|Z)', isi_hasil_output1)
@@ -238,5 +238,19 @@ def Code_splitter(inputFile):
         else :
             resultbeneranbanget.append(result[i])
             belom = False
+
+    idx = 0
+    belombelom = False
+    for i in range(len(resultbeneranbanget)):
+        if (resultbeneranbanget[idx]=='=' and not belombelom):
+            belombelom = True
+        elif(belombelom and resultbeneranbanget[idx]==' '):
+            resultbeneranbanget.pop(idx)
+            i+=1
+            idx -=1
+            belombelom = False
+        else :
+            belombelom = False
+        idx +=1;
 
     return resultbeneranbanget
