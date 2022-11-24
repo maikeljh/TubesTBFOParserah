2a7c30d3cd3fd2f5cf77f53dbd53a6771fc3569a
Enliven26 <16521443@mahasiswa.itb.ac.id>
2022-11-24 18:42:48 +0700

    loop statement fix

---
+++ testcase/test.js
@@ -0,0 +1,29 @@
+for (i =3; i++; a[54])
+{
+    while(true)
+    {
+        if (false)
+        {
+            break;
+            continue;
+        }
+        a++;
+        b.a = 4;
+        b[54];
+        continue;
+    }
+
+    switch(a)
+    {
+        case 5:
+            continue;
+    }
+
+    try {
+
+    }
+    catch(e)
+    {
+        break;
+    }
+}
