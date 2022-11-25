9b9302527fd4a4b769ddec09311e3c92b29b907b
Raylouiss <sebastianraynard@gmail.com>
2022-11-25 16:06:47 +0700

    ilangin \t

--- testcase/test.js
+++ testcase/test.js
@@ -1,8 +1,3 @@
-// program to create a multiline strings
-
-// using the + operator
-const message = 'This is a long message\n' + 
-    'that spans across multiple lines\n' + 
-    'in the code.';
-
-console.log(message);
+if (true) {
+                    console.log(b);
+}
